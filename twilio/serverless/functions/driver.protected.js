083e592fd4ffe80470cd62df567c8bf7b1a42dd9
Anthony Wong <anthonywong555@gmail.com>
2022-02-21 10:35:46 -0500

    Remove Twilio_Phone_Number in env so it can be dynamic.

--- twilio/serverless/functions/driver.protected.js
+++ twilio/serverless/functions/driver.protected.js
@@ -7,8 +7,8 @@
  */
 exports.handler = async(context, event, callback) => {
   try {
-    const {From, Body} = event;
-    const {ACCOUNT_SID, AUTH_TOKEN, TWILIO_STUDIO_FLOW, TWILIO_PHONE_NUMBER} = context;
+    const {From, Body, To} = event;
+    const {ACCOUNT_SID, AUTH_TOKEN, TWILIO_STUDIO_FLOW} = context;
     const twilioClient = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
 
     // Get all the MediaUrl##'s Value from the Event
@@ -27,7 +27,7 @@ exports.handler = async(context, event, callback) => {
     .create({
       parameters,
       to: From,
-      from: TWILIO_PHONE_NUMBER,
+      from: To,
     });
 
     return callback(null, null);
